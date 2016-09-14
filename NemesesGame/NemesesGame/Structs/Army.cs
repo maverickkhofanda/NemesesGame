2bb4d4e9b7eb1251e0ba0baf6c3c5d8497cddc18
greyfader <maverickkhofanda@gmail.com>
2016-09-14 18:11:01 +0700

    Added Army class

---
+++ NemesesGame/NemesesGame/Structs/Army.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NemesesGame;
+
+namespace NemesesGame
+{
+    public enum ArmyType { Regulars }
+    
+    public class Army
+    {
+        public Army ()
+        {
+            //init army_ dict
+            army_.Add(ArmyType.Regulars, 0);
+        }
+        
+        Dictionary<ArmyType, int> army_ = new Dictionary<ArmyType, int>();
+
+        public int Regulars { get { return army_[ArmyType.Regulars]; } set { army_[ArmyType.Regulars] = value; } }
+
+        public Dictionary<ArmyType, int> Army_ { get { return army_; } set { army_ = value; } }
+    }
+}
